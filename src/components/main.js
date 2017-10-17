06d1e254b3faa4b9672e4b21bc36d9f4bd2bb47c
jccho <jccho@c-maker.co.kr>
2017-10-17 16:28:42 +0900

    일반조사

--- src/components/main.js
+++ src/components/main.js
@@ -6,7 +6,7 @@ import surveyList from '../components/surveyList';
 import mySurvey from '../components/mySurvey'
 import pointHistory from '../components/pointHistory'
 import myPage from '../components/myPage'
-
+import renderIf from 'render-if'
 
 
 
@@ -34,13 +34,30 @@ export default class Main extends Component {
         }
         return (
             <Container>
-                <Header style={preSurveyFormStyle.headerLayout}>
+                <Header style={MainFormStyle.headerLayout}>
                     <View style={{flex:.1, alignItems: 'center'}}>
                         <Image source={require('../../assets/img/header_icon_alarm.png')} resizeMode={'contain'} style={{width:15, height:15, marginTop:5, marginLeft:10}}/>
                     </View>
+                    {renderIf(this.state.index == 0)(
                     <View style={{flex:.8, alignItems: 'center'}}>
                         <Image source={require('../../assets/img/header_icon_logo.png')} resizeMode={'contain'} style={{width:140, height:30, marginTop:5, marginLeft:10}}/>
                     </View>
+                    )}
+                    {renderIf(this.state.index == 1)(
+                        <View style={{flex:.8, alignItems: 'center'}}>
+                            <Text style={{fontSize:16}}>나의설문</Text>
+                        </View>
+                    )}
+                    {renderIf(this.state.index == 3)(
+                        <View style={{flex:.8, alignItems: 'center'}}>
+                            <Text style={{fontSize:16}}>포인트</Text>
+                        </View>
+                    )}
+                    {renderIf(this.state.index == 4)(
+                        <View style={{flex:.8, alignItems: 'center'}}>
+                            <Text style={{fontSize:16}}>마이페이지</Text>
+                        </View>
+                    )}
                     <View style={{flex:.1, alignItems: 'center'}}>
                         <Image source={require('../../assets/img/header_icon_set.png')} resizeMode={'contain'} style={{width:15, height:15, marginTop:5, marginLeft:10}}/>
                     </View>
@@ -54,7 +71,7 @@ export default class Main extends Component {
                 <Footer>
                     <FooterTab>
                         <Button onPress={() => this.switchScreen(0) }>
-                            <Image source={require('../../assets/img/surveyList_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>설문목록</Text>
+                            <Image source={require('../../assets/img/surveyList_icon_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={{color:"#DA4211"}}>설문목록</Text>
                         </Button>
                         <Button onPress={() => this.switchScreen(1) }>
                             <Image source={require('../../assets/img/mySurvey_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>나의설문</Text>
@@ -70,78 +87,10 @@ export default class Main extends Component {
 
             </Container>
         );
-        /*
-        return (
-            <Container>
-                <Header style={preSurveyFormStyle.headerLayout}>
-                    <View style={{flex:.1, alignItems: 'center'}}>
-                        <Image source={require('../../assets/img/header_icon_alarm.png')} resizeMode={'contain'} style={{width:15, height:15, marginTop:5, marginLeft:10}}/>
-                    </View>
-                    <View style={{flex:.8, alignItems: 'center'}}>
-                        <Image source={require('../../assets/img/header_icon_logo.png')} resizeMode={'contain'} style={{width:140, height:30, marginTop:5, marginLeft:10}}/>
-                    </View>
-                    <View style={{flex:.1, alignItems: 'center'}}>
-                        <Image source={require('../../assets/img/header_icon_set.png')} resizeMode={'contain'} style={{width:15, height:15, marginTop:5, marginLeft:10}}/>
-                    </View>
-                </Header>
-                <Content style={{padding:10}}>
-                    <View>
-                        <View style={preSurveyFormStyle.contentsLayout}>
-                            <View style={{flex:1, flexDirection: 'row', paddingTop:5, paddingBottom:5}}>
-                                <View style={{flex:0.25}}>
-                                    <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={preSurveyFormStyle.boldFont}>200P</Text>
-                                </View>
-                                <View style={{flex:0.65}}>
-                                    <Text style={preSurveyFormStyle.title}>설문 참여를 위한 사전 조사</Text>
-                                </View>
-                                <View style={{flex:0.1}}>
-                                    <Text>기본</Text>
-                                </View>
-                            </View>
-                            <View style={preSurveyFormStyle.lingBg}>
-                            </View>
-
-                            <View>
-                                <Text style={preSurveyFormStyle.contentsSize}>사전조사를 통해 입력하는 추가정보에 따라 참여할 수 있는 설문이 달라지며 잘못된 정보 입력으로 인해 설문보상 패널티를 받지 않게 주의하여 입력해주세요. 완료 이후 추가정보는 <Text style={preSurveyFormStyle.boldFont}>수정 불가능</Text>하며, 통계를 분석하는 용도로 활용됩니다.</Text>
-                            </View>
-                            <View style={{flexDirection: 'row', paddingLeft: 20,paddingTop:10}}>
-
-                                <View style={{backgroundColor: '#f6f6f6', flex: 0.3,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6",borderRightColor:"#f6f6f6"}} >
-                                    <Text style={{color:'#919191',fontSize:13}}>포인트적립</Text>
-                                </View>
-                                <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6"}}>
-                                    <Text style={preSurveyFormStyle.boldFont}>200P</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row',paddingLeft:20,paddingBottom:10}}>
-                                <View style={{backgroundColor: '#f6f6f6', flex: 0.3,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderRightColor:"#f6f6f6"}} >
-                                    <Text style={{color:'#919191',fontSize:13}}>응답시간</Text>
-                                </View>
-                                <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0"}}>
-                                    <Text style={{color:'#919191',fontSize:13}}>2분</Text>
-                                </View>
-                            </View>
-
-                            <View style={preSurveyFormStyle.lingBg}></View>
-                            <Button bordered full style={{borderColor:"#979797", backgroundColor:"#DA4211", justifyContent: 'center', paddingLeft:10}}>
-                                <Text style={{marginLeft:10, color:"#ffffff"}}>참여하기</Text>
-                            </Button>
-
-
-
-                        </View>
-
-                    </View>
-                </Content>
-
-            </Container>
-
-        );
-        */
     }
 }
 
-const preSurveyFormStyle = StyleSheet.create({
+const MainFormStyle = StyleSheet.create({
     headerLayout: {
         justifyContent: 'center', alignItems: 'center', backgroundColor: "#ffffff"
     }
--- src/components/surveyList.js
+++ src/components/surveyList.js
@@ -11,23 +11,23 @@ export default class surveyList extends Component {
         return (
             <ScrollView>
 
-                <View style={preSurveyFormStyle.contentsLayout}>
+                <View style={SurveyFormStyle.contentsLayout}>
                     <View style={{flex:1, flexDirection: 'row', paddingTop:5, paddingBottom:5}}>
                         <View style={{flex:0.25}}>
-                            <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={preSurveyFormStyle.boldFont}>200P</Text>
+                            <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={SurveyFormStyle.boldFont}>200P</Text>
                         </View>
                         <View style={{flex:0.65}}>
-                            <Text style={preSurveyFormStyle.title}>설문 참여를 위한 사전 조사</Text>
+                            <Text style={SurveyFormStyle.title}>설문 참여를 위한 사전 조사</Text>
                         </View>
                         <View style={{flex:0.1}}>
                             <Text>기본</Text>
                         </View>
                     </View>
-                    <View style={preSurveyFormStyle.lingBg}>
+                    <View style={SurveyFormStyle.lingBg}>
                     </View>
 
                     <View>
-                        <Text style={preSurveyFormStyle.contentsSize}>사전조사를 통해 입력하는 추가정보에 따라 참여할 수 있는 설문이 달라지며 잘못된 정보 입력으로 인해 설문보상 패널티를 받지 않게 주의하여 입력해주세요. 완료 이후 추가정보는 <Text style={preSurveyFormStyle.boldFont}>수정 불가능</Text>하며, 통계를 분석하는 용도로 활용됩니다.</Text>
+                        <Text style={SurveyFormStyle.contentsSize}>사전조사를 통해 입력하는 추가정보에 따라 참여할 수 있는 설문이 달라지며 잘못된 정보 입력으로 인해 설문보상 패널티를 받지 않게 주의하여 입력해주세요. 완료 이후 추가정보는 <Text style={SurveyFormStyle.boldFont}>수정 불가능</Text>하며, 통계를 분석하는 용도로 활용됩니다.</Text>
                     </View>
                     <View style={{flexDirection: 'row', paddingLeft: 20,paddingTop:10}}>
 
@@ -35,7 +35,7 @@ export default class surveyList extends Component {
                             <Text style={{color:'#919191',fontSize:13}}>포인트적립</Text>
                         </View>
                         <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6"}}>
-                            <Text style={preSurveyFormStyle.boldFont}>200P</Text>
+                            <Text style={SurveyFormStyle.boldFont}>200P</Text>
                         </View>
                     </View>
                     <View style={{flexDirection: 'row',paddingLeft:20,paddingBottom:10}}>
@@ -47,28 +47,28 @@ export default class surveyList extends Component {
                         </View>
                     </View>
 
-                    <View style={preSurveyFormStyle.lingBg}></View>
+                    <View style={SurveyFormStyle.lingBg}></View>
                     <Button bordered full style={{borderColor:"#979797", backgroundColor:"#DA4211", justifyContent: 'center', paddingLeft:10}}>
                         <Text style={{color:"#ffffff"}} onPress={Actions.PreSurvey}>참여하기</Text>
                     </Button>
                 </View>
 
-                <View style={preSurveyFormStyle.contentsLayout}>
+                <View style={SurveyFormStyle.contentsLayout}>
                     <View style={{flex:1, flexDirection: 'row', paddingTop:10, paddingBottom:5}}>
                         <View style={{flex:0.25}}>
-                            <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={preSurveyFormStyle.boldFont}>800P</Text>
+                            <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={SurveyFormStyle.boldFont}>800P</Text>
                         </View>
                         <View style={{flex:0.65}}>
-                            <Text style={preSurveyFormStyle.title}>2017 맥주 브랜드에 대한 이미지 조사</Text>
+                            <Text style={SurveyFormStyle.title}>2017 맥주 브랜드에 대한 이미지 조사</Text>
                         </View>
                         <View style={{flex:0.1}}>
                             <Text>맥주</Text>
                         </View>
                     </View>
-                    <View style={preSurveyFormStyle.lingBg}>
+                    <View style={SurveyFormStyle.lingBg}>
                     </View>
                     <View>
-                        <Text style={preSurveyFormStyle.contentsSize}>본 설문조사는 회원님의 소중한 의견을 듣고자 진행하는 일반적인 설문조사입니다.</Text>
+                        <Text style={SurveyFormStyle.contentsSize}>본 설문조사는 회원님의 소중한 의견을 듣고자 진행하는 일반적인 설문조사입니다.</Text>
                     </View>
                     <View style={{flexDirection: 'row', paddingLeft: 20,paddingTop:10}}>
 
@@ -76,7 +76,7 @@ export default class surveyList extends Component {
                             <Text style={{color:'#919191',fontSize:13}}>포인트적립</Text>
                         </View>
                         <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6"}}>
-                            <Text style={preSurveyFormStyle.boldFont}>10~800P</Text>
+                            <Text style={SurveyFormStyle.boldFont}>10~800P</Text>
                         </View>
                     </View>
                     <View style={{flexDirection: 'row',paddingLeft:20}}>
@@ -95,7 +95,7 @@ export default class surveyList extends Component {
                             <Text style={{color:'#919191',fontSize:13}}>5000명 (1238명 참여)</Text>
                         </View>
                     </View>
-                    <View style={preSurveyFormStyle.lingBg}></View>
+                    <View style={SurveyFormStyle.lingBg}></View>
                     <View style={{flex:1, flexDirection: 'row', paddingTop:5, paddingBottom:5}}>
                         <View style={{flex:0.65}}>
                             <Text>2017.10.21 ~ 완료시까지</Text>
@@ -114,7 +114,7 @@ export default class surveyList extends Component {
     };
 }
 
-const preSurveyFormStyle = StyleSheet.create({
+const SurveyFormStyle = StyleSheet.create({
     headerLayout: {
         justifyContent: 'center', alignItems: 'center', backgroundColor: "#ffffff"
     }
