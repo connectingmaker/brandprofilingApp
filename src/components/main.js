2b34de88563b323650d994e3bc1a58e27e8cebc2
jccho <jccho@c-maker.co.kr>
2017-10-17 16:42:54 +0900

    일반조사

--- src/components/main.js
+++ src/components/main.js
@@ -69,6 +69,7 @@ export default class Main extends Component {
 
                 </Content>
                 <Footer>
+                    {renderIf(this.state.index == 0)(
                     <FooterTab>
                         <Button onPress={() => this.switchScreen(0) }>
                             <Image source={require('../../assets/img/surveyList_icon_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={{color:"#DA4211"}}>설문목록</Text>
@@ -82,7 +83,60 @@ export default class Main extends Component {
                         <Button onPress={() => this.switchScreen(4) }>
                             <Image source={require('../../assets/img/myPage_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>마이페이지</Text>
                         </Button>
+
                     </FooterTab>
+                    )}
+                    {renderIf(this.state.index == 1)(
+                        <FooterTab>
+                            <Button onPress={() => this.switchScreen(0) }>
+                                <Image source={require('../../assets/img/surveyList_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={{color:"#DA4211"}}>설문목록</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(1) }>
+                                <Image source={require('../../assets/img/mySurvey_icon_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>나의설문</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(3) }>
+                                <Image source={require('../../assets/img/point_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>포인트</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(4) }>
+                                <Image source={require('../../assets/img/myPage_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>마이페이지</Text>
+                            </Button>
+
+                        </FooterTab>
+                    )}
+                    {renderIf(this.state.index == 3)(
+                        <FooterTab>
+                            <Button onPress={() => this.switchScreen(0) }>
+                                <Image source={require('../../assets/img/surveyList_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={{color:"#DA4211"}}>설문목록</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(1) }>
+                                <Image source={require('../../assets/img/mySurvey_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>나의설문</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(3) }>
+                                <Image source={require('../../assets/img/point_icon_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>포인트</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(4) }>
+                                <Image source={require('../../assets/img/myPage_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>마이페이지</Text>
+                            </Button>
+
+                        </FooterTab>
+                    )}
+                    {renderIf(this.state.index == 4)(
+                        <FooterTab>
+                            <Button onPress={() => this.switchScreen(0) }>
+                                <Image source={require('../../assets/img/surveyList_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={{color:"#DA4211"}}>설문목록</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(1) }>
+                                <Image source={require('../../assets/img/mySurvey_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>나의설문</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(3) }>
+                                <Image source={require('../../assets/img/point_icon_off.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>포인트</Text>
+                            </Button>
+                            <Button onPress={() => this.switchScreen(4) }>
+                                <Image source={require('../../assets/img/myPage_icon_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text>마이페이지</Text>
+                            </Button>
+
+                        </FooterTab>
+                    )}
                 </Footer>
 
             </Container>
--- src/components/mySurvey.js
+++ src/components/mySurvey.js
@@ -12,48 +12,6 @@ export default class mySurvey extends Component {
             <ScrollView>
 
                 <View style={SurveyFormStyle.contentsLayout}>
-                    <View style={{flex:1, flexDirection: 'row', paddingTop:5, paddingBottom:5}}>
-                        <View style={{flex:0.25}}>
-                            <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={SurveyFormStyle.boldFont}>200P</Text>
-                        </View>
-                        <View style={{flex:0.65}}>
-                            <Text style={SurveyFormStyle.title}>설문 참여를 위한 사전 조사</Text>
-                        </View>
-                        <View style={{flex:0.1}}>
-                            <Text>기본</Text>
-                        </View>
-                    </View>
-                    <View style={SurveyFormStyle.lingBg}>
-                    </View>
-
-                    <View>
-                        <Text style={SurveyFormStyle.contentsSize}>사전조사를 통해 입력하는 추가정보에 따라 참여할 수 있는 설문이 달라지며 잘못된 정보 입력으로 인해 설문보상 패널티를 받지 않게 주의하여 입력해주세요. 완료 이후 추가정보는 <Text style={SurveyFormStyle.boldFont}>수정 불가능</Text>하며, 통계를 분석하는 용도로 활용됩니다.</Text>
-                    </View>
-                    <View style={{flexDirection: 'row', paddingLeft: 20,paddingTop:10}}>
-
-                        <View style={{backgroundColor: '#f6f6f6', flex: 0.3,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6",borderRightColor:"#f6f6f6"}} >
-                            <Text style={{color:'#919191',fontSize:13}}>포인트적립</Text>
-                        </View>
-                        <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderBottomColor:"#f6f6f6"}}>
-                            <Text style={SurveyFormStyle.boldFont}>200P</Text>
-                        </View>
-                    </View>
-                    <View style={{flexDirection: 'row',paddingLeft:20,paddingBottom:10}}>
-                        <View style={{backgroundColor: '#f6f6f6', flex: 0.3,padding:10,borderWidth:1,borderColor:"#d0d0d0",borderRightColor:"#f6f6f6"}} >
-                            <Text style={{color:'#919191',fontSize:13}}>응답시간</Text>
-                        </View>
-                        <View style={{borderColor: '#d0d0d0', flex: 0.5,padding:10,borderWidth:1,borderColor:"#d0d0d0"}}>
-                            <Text style={{color:'#919191',fontSize:13}}>2분</Text>
-                        </View>
-                    </View>
-
-                    <View style={SurveyFormStyle.lingBg}></View>
-                    <Button bordered full style={{borderColor:"#979797", backgroundColor:"#DA4211", justifyContent: 'center', paddingLeft:10}}>
-                        <Text style={{color:"#ffffff"}} onPress={Actions.PreSurvey}>참여하기</Text>
-                    </Button>
-                </View>
-
-                <View style={SurveyFormStyle.contentsLayout}>
                     <View style={{flex:1, flexDirection: 'row', paddingTop:10, paddingBottom:5}}>
                         <View style={{flex:0.25}}>
                             <Image source={require('../../assets/img/main_icon_logo_on.png')} resizeMode={'contain'} style={{width:30,height:30, marginTop:5}}/><Text style={SurveyFormStyle.boldFont}>800P</Text>
